3c7fe4699e3b811f5a8d79312d5d953615fa3a81
kayemk <contact@kayemk.net>
2019-01-18 16:26:14 +0100

    Ci.

--- ci/build.sh
+++ ci/build.sh
@@ -3,4 +3,17 @@
 set -e -x
 
   echo $(whoami)
+  ls -ahl
   apk --update add openssl-dev pcre-dev zlib-dev wget build-base
+  ./configure \
+        --with-http_ssl_module \
+        --with-http_gzip_static_module \
+        --prefix=/etc/nginx \
+        --http-log-path=/var/log/nginx/access.log \
+        --error-log-path=/var/log/nginx/error.log \
+        --sbin-path=/usr/local/sbin/nginx && \
+    make && \
+    make install && \
+    apk del build-base && \
+    rm -rf /tmp/src && \
+    rm -rf /var/cache/apk/*
